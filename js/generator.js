d798a05a5b715f4b41194cbdad5291dd397366e1
AlexCaos75 <alessandro75.ar@gmail.com>
2026-05-15 21:14:08 +0200

    Update generator.js

--- js/generator.js
+++ js/generator.js
@@ -26,27 +26,34 @@ if (gifFileInput) {
       const reader =
         new FileReader();
 
-reader.onload = function(ev){
+      reader.onload = function(ev){
 
-  gifDataURL =
-    ev.target.result;
+        gifDataURL =
+          ev.target.result;
 
-  const gifUrlInput =
-    document.getElementById("gifurl");
+        const gifUrlInput =
+          document.getElementById("gifurl");
 
-  if (gifUrlInput) {
-    gifUrlInput.value = "";
-  }
+        if (gifUrlInput) {
+          gifUrlInput.value = "";
+        }
 
-  console.log(
-    "GIF DATAURL LOADED"
+        console.log(
+          "GIF DATAURL LOADED"
+        );
+
+        genera();
+
+        e.target.value = "";
+
+      };
+
+      reader.readAsDataURL(file);
+
+    }
   );
 
-  genera();
-
-  e.target.value = "";
-
-};
+}
 /* =========================
    MINI FILE UPLOAD
 ========================= */

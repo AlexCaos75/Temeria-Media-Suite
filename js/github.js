17439e617724b4388eb773315564fa1e348fe156
AlexCaos75 <alessandro75.ar@gmail.com>
2026-05-23 03:18:51 +0200

    Update github.js

--- js/github.js
+++ js/github.js
@@ -194,15 +194,10 @@ if(!video.startsWith("data:video/")){
   console.warn("[TEMERIA VIDEO] Formato video non valido:", video.slice(0,80));
   return "";
 }
-let ext = "mp4";
+  let ext = "mp4";
 
-/*
-  Forziamo MP4 per massima compatibilità autoplay/browser.
-*/
-
-if(video.includes("webm")){
-  ext = "webm";
-}
+if(video.includes("webm")) ext = "webm";
+if(video.includes("mov")) ext = "mov";
   const base64 = extractBase64(video);
 
   if(!base64){
